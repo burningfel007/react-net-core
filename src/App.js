f9468e461f49c2cb831ddd3769d78736469b71fc
Marlon Santos <burningfel007@gmail.com>
2024-05-25 16:21:17 -0600

    REACT-NET-CORE-FRONT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { Card, Container, Col, Row, CardBody, CardHeader, Button } from 'reactstrap';
+import { useEffect, useState } from 'react';
 import './App.css';
+import TablaContacto from './Componentes/TablaContacto';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [contactos, setContactos] = useState([]);
+  const mostrarContactos = async()=>{
+    const response = await fetch("https://localhost:7232/api/Contacto/Lista");
+    if(response.ok){
+      const data = await response.json();
+      setContactos(data);
+    }else{
+      console.log("Error en la respuesta");
+    }
+  }
+  useEffect(()=>{
+    mostrarContactos();
+  },[])
+    return (
+        <Container>
+            <Row className="mt-5">
+                <Col>
+                    <Card>
+                        <CardHeader>
+                            <h5>Lista de contactos</h5>
+                        </CardHeader>
+                        <CardBody>
+                            <Button size="sm" color="success">Nuevo Contacto</Button>
+                            <hr />
+                            <TablaContacto data={contactos}/>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
